3192a0879611eb5ff407615c51585d83fb5468d8
Stephen Cassels <scassels@findmypast.com>
2016-04-26 09:28:18 +0100

    wip - acronym

--- acronym/acronym.exs
+++ acronym/acronym.exs
@@ -1,10 +1,23 @@
 defmodule Acronym do
   @doc """
-  Generate an acronym from a string. 
+  Generate an acronym from a string.
   "This is a string" => "TIAS"
   """
   @spec abbreviate(string) :: String.t()
   def abbreviate(string) do
-   
+    acronym = []
+    string
+    |> ~W
+    |> String.to_char_list()
+    |> take_first_letter_from_word(acronym)
+  end
+
+  defp take_first_letter_from_word(acronym, head|tail) do
+      acronym = [List.first(head)] ++ acronym
+      take_first_letter_from_word(acronym, tail)
+  end
+
+  defp take_first_letter_from_word(acronym, []) do
+      acronym
   end
 end
