720da13b07824f21e3bf00b76b032d6782aa5b1f
Stephen Cassels <steve.o.cassels@gmail.com>
2017-02-04 08:47:45 +0000

    Move Elixir exercisms to elixir folder

---
+++ elixir/bob/bob.exs
@@ -0,0 +1,49 @@
+defmodule Bob do
+@moduledoc """
+  Returns appropriate response for the rules in the readme.
+"""
+
+  @spec hey(String.t) :: String.t
+  def hey(input) do
+    cond do
+      saying_nothing?(input) ->
+        "Fine. Be that way!"
+      yelling?(input) ->
+        "Whoa, chill out!"
+      questioning?(input) ->
+        "Sure."
+      true -> "Whatever."
+    end
+  end
+
+  defp saying_nothing?(input) do
+    input
+    |> strip_char(" ")
+    |> nothing?()
+  end
+
+  defp yelling?(input) do
+    cleaned =
+      input
+      |> strip_char(", ")
+
+    is_all_number = fn(x) -> x =~ ~r/^\d+$/ end
+    is_upper_case = fn(x) -> String.upcase(x) == x end
+
+    !is_all_number.(cleaned) &&
+    is_upper_case.(cleaned) &&
+    !questioning?(cleaned)
+  end
+
+  defp strip_char(input, char) do
+    String.replace(input, char, "")
+  end
+
+  defp questioning?(input) do
+    String.ends_with?(input, "?")
+  end
+
+  defp nothing?(input) do
+    String.length(input) == 0
+  end
+end
